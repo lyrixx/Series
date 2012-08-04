46f135d1bb06c229a0724e6b11c4b223327768cd
Grégoire Pineau <lyrixx@lyrixx.info>
2012-08-04 18:25:17 +0200

    Added Series/Provider/Upstream/TorrenthoundTorrentProvider

---
+++ Series/Tests/Provider/Upstream/TorrenthoundTorrentProviderTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Series\Tests\Provider\Upstream;
+
+use Series\Provider\Upstream\TorrenthoundTorrentProvider;
+use Series\Show\Upstream\ShowCollection;
+
+class TorrenthoundTorrentProviderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testFetch()
+    {
+        $response = new \Buzz\Message\Response();
+        $response->setContent(file_get_contents(__DIR__.'/Fixtures/Torrenthound.xml'));
+        $response->setHeaders(array('HTTP/1.1 200 OK'));
+
+        $browser = $this->getMock('Buzz\Browser');
+        $browser
+            ->expects($this->once())
+            ->method('get')
+            ->will($this->returnValue($response))
+        ;
+
+        $showCollection = new ShowCollection();
+        $provider = new TorrenthoundTorrentProvider($browser, '\\Series\\Show\\Upstream\\ShowTorrent', $showCollection);
+        $provider->fetch();
+
+        $this->assertCount(6, $showCollection->getShows());
+    }
+
+}
